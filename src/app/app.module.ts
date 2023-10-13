6ea9b393ebf5f382a1617c89b9290e9d5cfdc21c
AditiaW <aditiaw1507@gmail.com>
2023-10-13 10:29:35 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RegisterComponent } from './register/register.component';
+import { AdminComponent } from './admin/admin.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+
+const routes: Routes = [
+  { path: "admin", component: AdminComponent },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent }, // Tambahkan rute untuk Register
+  { path: "", redirectTo: "login", pathMatch: "full" }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    RegisterComponent,
+    AdminComponent,
+    HeaderComponent,
+    SidebarComponent,
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
